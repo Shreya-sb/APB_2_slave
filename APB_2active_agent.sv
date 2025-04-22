bc0885c841c4a234a46473f5f7e4afce34ad4e5b
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-04-22 12:37:38 +0530

    Update APB_2active_agent.sv

--- APB_2active_agent.sv
+++ APB_2active_agent.sv
@@ -3,6 +3,7 @@ class APB_2active_agent extends uvm_agent;
   `uvm_component_utils(APB_2active_agent)
   APB_2sequencer APB_2seq;
   APB_2driver APB_2dri;
+  APB_2monitor_1 APB_2mon_1;
  
   function new(string name = "APB_2active_agent", uvm_component parent);
     super.new(name, parent);
@@ -10,8 +11,9 @@ class APB_2active_agent extends uvm_agent;
  
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-     APB_2seq = APB_2sequencer::type_id::create("APB_2seq", this);
+    APB_2seq = APB_2sequencer::type_id::create("APB_2seq", this);
     APB_2dri = APB_2driver::type_id::create("APB_2dri", this);
+    APB_2mon_1 = APB_2monitor_1::type_id::create("APB_2mon_1", this);  
   endfunction
  
   function void connect_phase(uvm_phase phase);
