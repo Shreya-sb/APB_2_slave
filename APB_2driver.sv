4b7cb9e4fbd9a984a234aa03066614eb0b8f9b09
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-04-25 14:03:53 +0530

    Update APB_2driver.sv

--- APB_2driver.sv
+++ APB_2driver.sv
@@ -7,11 +7,9 @@ class APB_2driver extends uvm_driver#(APB_2sequence_item);
   virtual APB_2interface.APB_DRV vif;
   `uvm_component_utils(APB_2driver)
 
-  uvm_analysis_port #(APB_2sequence_item) item_collect_port;
  
   function new(string name = "APB_2driver", uvm_component parent);
     super.new(name,parent);
-	item_collect_port = new("item_collect_port",this);
   endfunction
  
   function void build_phase(uvm_phase phase);
