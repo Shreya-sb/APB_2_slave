b71a335dea9e1721fb83d790ed59e2605d8d590b
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-05-14 14:16:04 +0530

    Update APB_2monitor_2.sv

--- APB_2monitor_2.sv
+++ APB_2monitor_2.sv
@@ -23,6 +23,7 @@ class APB_2monitor_2 extends uvm_monitor;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     item_collect_port2 = new("item_collect_port2", this);
+    APB_2mon_item = APB_2sequence_item::type_id::create("APB_2mon_item", this);
     if (!uvm_config_db#(virtual APB_2interface)::get(this, "", "vif", vif))
       `uvm_fatal("APB_2monitor_2", "Virtual interface not set for APB_2monitor_2");
   endfunction
@@ -35,19 +36,16 @@ class APB_2monitor_2 extends uvm_monitor;
       @(vif.APB_2monitor_cb)
       
       // Sample DUT information and populate the transaction
-      APB_2mon_item = APB_2sequence_item::type_id::create("APB_2mon_item", this);
       APB_2mon_item.transfer         = vif.APB_2monitor_cb.transfer;
       APB_2mon_item.READ_WRITE       = vif.APB_2monitor_cb.READ_WRITE;
       APB_2mon_item.apb_write_paddr  = vif.APB_2monitor_cb.apb_write_paddr;
       APB_2mon_item.apb_write_data   = vif.APB_2monitor_cb.apb_write_data;
       APB_2mon_item.apb_read_paddr   = vif.APB_2monitor_cb.apb_read_paddr;
       APB_2mon_item.apb_read_data_out= vif.APB_2monitor_cb.apb_read_data_out;
-      `uvm_info("MONITOR_2", $sformatf("----Start of Monitor_2----"), UVM_LOW);
-
       
       // Broadcast the transaction using the analysis port
       item_collect_port2.write(APB_2mon_item);
-      
+      `uvm_info("MONITOR_2", $sformatf("----Start of Monitor_2----"), UVM_LOW);      
       `uvm_info("MONITOR_2",$sformatf("transfer=%0d READ_WRITE=%0d apb_write_paddr=%0d,apb_write_data=%0d,apb_read_paddr=%0d,apb_read_data_out=%0d",APB_2mon_item.transfer,APB_2mon_item.READ_WRITE,APB_2mon_item.apb_write_paddr,APB_2mon_item.apb_write_data,APB_2mon_item.apb_read_paddr,APB_2mon_item.apb_read_data_out),UVM_LOW)
 	  //repeat(2)@(vif.APB_2monitor_cb); 
     end
