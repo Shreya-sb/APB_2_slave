ecd52e8c943c88ecdf462c20c99b3a209cca7f6d
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-05-14 14:55:14 +0530

    Updated APB_2scoreboard.sv

--- APB_2scoreboard.sv
+++ APB_2scoreboard.sv
@@ -38,7 +38,7 @@ class APB_2scoreboard extends uvm_scoreboard;
   //Define build phase
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-     item_collect1_export = new("item_collect1_export", this);
+    item_collect1_export = new("item_collect1_export", this);
     item_collect2_export = new("item_collect2_export", this);
 
     if (!uvm_config_db#(virtual APB_2interface)::get(this, "", "vif", vif)) begin
@@ -48,14 +48,20 @@ class APB_2scoreboard extends uvm_scoreboard;
 
   //Write methods to capture transactions from monitors
   function void write_monitor1port(APB_2sequence_item input_trans);
-    $display("Entered monitor1 write");
+    $display("Enteredwrite");
     input_queue.push_back(input_trans);
+    `uvm_info("SB", $sformatf(
+      "EXPECTED TRANSACTION: size = %0d | TRANSFER = %0b | RW = %0b | WDATA = %0h | WPADDR = %0h | RPADDR = %0h",
+      input_queue.size(), input_trans.transfer, input_trans.READ_WRITE, input_trans.apb_write_data, input_trans.apb_write_paddr, input_trans.apb_read_paddr), UVM_LOW)
   endfunction
   
  
   function void write_monitor2port(APB_2sequence_item output_trans);
     $display("Entered monitor2 write");
     output_queue.push_back(output_trans);
+    "ACTUAL TRANSACTION: size = %0d | TRANSFER = %0b | RW = %0b | WPADDR = %0h | WDATA = %0h | RDATA = %0h | RPADDR = %0h",
+      output_queue.size(), output_trans.transfer, output_trans.READ_WRITE, output_trans.apb_write_paddr, output_trans.apb_write_data, output_trans.apb_read_data_out, output_trans.apb_read_paddr), UVM_LOW)
+  endfunction
   endfunction
   
     // Comparison logic
@@ -111,7 +117,7 @@ function void compare_results(APB_2sequence_item input_trans,APB_2sequence_item 
                     monitor1_trans.apb_read_data_out,monitor2_trans.apb_read_data_out,monitor1_trans.apb_read_paddr,monitor2_trans.apb_read_paddr ), UVM_LOW)
          monitor2_trans.print();
 
-        $display("-------------------Test PASS---------------------\n");
+        //$display("-------------------Test PASS---------------------\n");
       end
     else begin
       mismatch++;
@@ -120,7 +126,7 @@ function void compare_results(APB_2sequence_item input_trans,APB_2sequence_item 
       `uvm_error("COMPARE", $sformatf("READ PASS | EXPECTED RDATA = %0h  ACTUAL RDATA =%0h |EXPECTED  READ ADDR = %0h ACTUAL READ ADDR =%0h",
                     monitor1_trans.apb_read_data_out,monitor2_trans.apb_read_data_out,monitor1_trans.apb_read_paddr,monitor2_trans.apb_read_paddr ))
        monitor2_trans.print();
-      $display("-------------------Test FAIL---------------------\n");
+      //$display("-------------------Test FAIL---------------------\n");
     end
    end
    else
@@ -133,7 +139,7 @@ function void compare_results(APB_2sequence_item input_trans,APB_2sequence_item 
         `uvm_info("COMPARE", $sformatf("WRITE PASS | EXPECTED WDATA = %0h  ACTUAL WDATA =%0h |EXPECTED  WRITE ADDR = %0h ACTUAL WRITE ADDR =%0h",
                     monitor1_trans.apb_write_data,monitor2_trans.apb_write_data,monitor1_trans.apb_write_paddr,monitor2_trans.apb_write_paddr ), UVM_LOW)
           monitor2_trans.print();
-        $display("-------------------Test PASS---------------------\n");
+        //$display("-------------------Test PASS---------------------\n");
       end
     else begin
       mismatch++;
@@ -142,7 +148,7 @@ function void compare_results(APB_2sequence_item input_trans,APB_2sequence_item 
       `uvm_error("COMPARE", $sformatf("WRITE FAIL | EXPECTED WDATA = %0h  ACTUAL WDATA =%0h |EXPECTED  WRITE ADDR = %0h ACTUAL WRITE ADDR =%0h",
                     monitor1_trans.apb_write_data,monitor2_trans.apb_write_data,monitor1_trans.apb_write_paddr,monitor2_trans.apb_write_paddr ))
        monitor2_trans.print();
-      $display("-------------------Test FAIL---------------------\n");
+      //$display("-------------------Test FAIL---------------------\n");
     end
 
     end
@@ -151,22 +157,31 @@ function void compare_results(APB_2sequence_item input_trans,APB_2sequence_item 
      
     // Main run phase
   task run_phase (uvm_phase phase);
+     super.run_phase(phase);
     forever begin
       wait(input_queue.size() >0 && output_queue.size() >0);
       monitor1_trans = input_queue.pop_front();
       monitor2_trans = output_queue.pop_front();
       
       if(monitor1_trans.transfer)begin
-        if(monitor1_trans.READ_WRITE)begin
+        if(monitor1_trans.READ_WRITE==0)begin
           mem_model[monitor1_trans.apb_write_paddr] = monitor1_trans.apb_write_data;
           //compare_results(monitor1_trans, monitor2_trans);
         end
         else begin
           monitor1_trans.apb_read_data_out = mem_model[monitor1_trans.apb_read_paddr];
         end
-                  compare_results(monitor1_trans,monitor2_trans);
+          compare_results(monitor1_trans,monitor2_trans);
 
       end
     end
   endtask
+
+  function void report_phase(uvm_phase phase);
+    super.report_phase(phase);
+    `uvm_info("SCOREBOARD", "-----------------------------", UVM_NONE)
+    `uvm_info("SCOREBOARD", $sformatf("PASSED: %0d", match), UVM_NONE)
+    `uvm_info("SCOREBOARD", $sformatf("FAILED: %0d", mismatch), UVM_NONE)
+    `uvm_info("SCOREBOARD", "-----------------------------", UVM_NONE)
+  endfunction
 endclass
